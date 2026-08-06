5b58f68c2502bc1c82585ace58cc7a2815a5567c
Jose Wanner <wanch25@gmail.com>
2026-08-05 22:01:46 -0600

    fix: update styles and dependencies

--- frontend/sistema-smi-frontend/src/App.jsx
+++ frontend/sistema-smi-frontend/src/App.jsx
@@ -20,6 +20,9 @@ export default function App() {
   // ESTADOS GLOBALES DE PRODUCTO, PAÍSES Y ORIGEN
   // ----------------------------------------------------
   const [productoSeleccionado, setProductoSeleccionado] = useState(null);
+  useEffect(() => {
+  console.log("APP productoSeleccionado:", productoSeleccionado);
+  }, [productoSeleccionado]);
   const [paisesDestino, setPaisesDestino] = useState([]);
   const [paisOrigen, setPaisOrigen] = useState('España'); // Por defecto
 
