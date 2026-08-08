62654243cb8dcb258c2cb173f9e13feabb76a820
Jose Wanner <wanch25@gmail.com>
2026-08-07 18:37:18 -0600

    fix: update styles and dependencies

--- frontend/sistema-smi-frontend/src/components/TabProductos.jsx
+++ frontend/sistema-smi-frontend/src/components/TabProductos.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient.js';
+import { THEME } from '../theme';
 
 export default function TablaProductos({
   paisDestino,
