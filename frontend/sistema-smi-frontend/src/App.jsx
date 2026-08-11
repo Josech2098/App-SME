158ab35896ed1829cc79c828f9e1c95dc2e0e31a
Jose Wanner <wanch25@gmail.com>
2026-08-11 16:00:49 -0600

    fix: update styles and dependencies

--- frontend/sistema-smi-frontend/src/App.jsx
+++ frontend/sistema-smi-frontend/src/App.jsx
@@ -373,7 +373,7 @@ export default function App() {
       <main className="flex-1 p-8 overflow-y-auto" onClick={() => { setIsDropdownOpen(false); setIsCatDropdownOpen(false); }}>
         
         <h1 className="text-3xl font-bold text-white mb-6 tracking-tight">
-          Aplicativo Selección de Mercados Internacionales Para La Exportación de Mercancías
+          Aplicativo selección de mercados internacionales para la exportación de mercancías
         </h1>
 
         {/* Pestañas (Tabs) */}
