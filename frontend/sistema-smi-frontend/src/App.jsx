e4e3e7fbcbf081e8cdf569ba75eacf97322caf8e
Jose Wanner <wanch25@gmail.com>
2026-08-11 12:17:13 -0600

    fix: update styles and dependencies

--- frontend/sistema-smi-frontend/src/App.jsx
+++ frontend/sistema-smi-frontend/src/App.jsx
@@ -354,7 +354,7 @@ export default function App() {
             <img 
               src={logoSmipem} 
               alt="SMIPEM Logo" 
-              className="h-16 w-auto object-contain mb-1" 
+              className="h-20 w-auto object-contain mb-1" 
             />
             <p className="text-sm">
               Creador: Jose Jaime Baena Rojas
